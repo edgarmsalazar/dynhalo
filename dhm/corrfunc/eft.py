86fbde1a7d5b31ddda2574e3dc87e9a622d8298a
Edgar Salazar <edgarmsc@arizona.edu>
2024-02-19 11:35:55 -0700

    added cost functions for optimizing xi large parameters

--- dhm/corrfunc/eft.py
+++ dhm/corrfunc/eft.py
@@ -82,20 +82,43 @@ def eft_counter_term_corr_func_prediction(klin, plin, cs=0) -> Tuple[np.ndarray]
     if cs != 0:
         k_factor = kcleft**2 / (1 + kcleft**2)
         lptpk += cs * k_factor * cterm
-        
+
     eftpred = loginterp(kcleft, lptpk)(klin)
     r_eft, xi_eft = sph.sph(0, eftpred)
 
     return r_eft, xi_eft[0]
 
 
+def power_spec_box_effect(k, pk, boxsize, lamb):
+    rbox = boxsize * np.cbrt(3. / 4. / np.pi)
+    phat = (1 - np.exp(-lamb * (rbox * k) ** 2)) * pk
+    return phat
+
+
 def loglike_cs(cs, data) -> float:
     # Check prior
-    if not cs > 0:
+    if cs < 0:
         return -np.inf
 
+    # Unpack data
     k, pk, r, xi, cov = data
     xi_pred = interp1d(*eft_counter_term_corr_func_prediction(k, pk, cs=cs))
+    # Compute chi2
+    d = xi - xi_pred(r)
+    return -np.dot(d, np.linalg.solve(cov, d))
+
+
+def loglike_lamb(lamb, data) -> float:
+    # Check priors
+    if lamb < 0:
+        return -np.inf
+    # Unpack data
+
+    k, pk, r, xi, cov, cs, boxsize = data
+    # Account for the simulation box size in the linear power spectrum
+    phat = power_spec_box_effect(k, pk, boxsize, lamb)
+    # Compute chi2
+    xi_pred = interp1d(*eft_counter_term_corr_func_prediction(k, phat, cs=cs))
     d = xi - xi_pred(r)
     return -np.dot(d, np.linalg.solve(cov, d))
 
@@ -118,6 +141,7 @@ def xi_large_estimation(
     Omb: float,
     ns: float,
     sigma8: float,
+    boxsize: float,
     z: float = 0,
     large_only: bool = True,
     power_spectra: bool = False,
@@ -134,23 +158,46 @@ def xi_large_estimation(
 
     # Find the best value of cs that minimizes the eft prediction error at
     # intermediate scales
+    # ==========================================================================
     r_mask = (40 < r) & (r < 80)
     args = (k_lin, p_lin, r[r_mask], xi[r_mask], xi_cov[r_mask, :][:, r_mask])
     # Define grids to estimate cs
-    ngrid = 20
-    grid = np.linspace(0.5 * 4.5, 5 * 4.5, ngrid)
-    # cost_ = loglike_cs(4.5, args)
+    ngrid = 16
+    grid = np.logspace(0, 1.2, ngrid)
     with Pool(16) as p:
         loglike_grid = p.starmap(loglike_cs, zip(grid, repeat(args, ngrid)))
     cs_max = grid[np.argmax(loglike_grid)]
-    ngrid = 100
-    grid = np.linspace(cs_max - 0.25, cs_max + 0.25, 100)
+    # Refine grid around cs_max with 10% deviation
+    ngrid = 80
+    grid = np.linspace(0.9*cs_max, 1.1*cs_max, ngrid)
     with Pool(16) as p:
         loglike_grid = p.starmap(loglike_cs, zip(grid, repeat(args, ngrid)))
     cs_max = grid[np.argmax(loglike_grid)]
+    # ==========================================================================
+
+    # Account for the box size effects on large scale Fourier modes
+    # ==========================================================================
+    r_mask = (40 < r) & (r < 150)
+    args = (k_lin, p_lin, r[r_mask], xi[r_mask], xi_cov[r_mask, :][:, r_mask],
+            cs_max, boxsize)
+    # Define grids to estimate cs
+    ngrid = 16
+    grid = np.logspace(-1.5, 0, ngrid)
+    with Pool(16) as p:
+        loglike_grid = p.starmap(loglike_lamb, zip(grid, repeat(args, ngrid)))
+    lamb_max = grid[np.argmax(loglike_grid)]
+    # Refine grid around cs_max with 10% deviation below and 50% above
+    ngrid = 80
+    grid = np.linspace(0.9*lamb_max, 1.5*lamb_max, ngrid)
+    with Pool(16) as p:
+        loglike_grid = p.starmap(loglike_lamb, zip(grid, repeat(args, ngrid)))
+    lamb_max = grid[np.argmax(loglike_grid)]
+    # ==========================================================================
 
     # Compute the 1-loop EFT approx.
-    r_eft, xi_eft = eft_counter_term_corr_func_prediction(k_lin, p_lin, cs=cs_max)
+    phat = power_spec_box_effect(k_lin, p_lin, boxsize, lamb_max)
+    r_eft, xi_eft = eft_counter_term_corr_func_prediction(
+        k_lin, phat, cs=cs_max)
 
     # Evaluate ZA in the same grid as EFT
     p_zel = pk_zel_call(k_lin)
@@ -158,14 +205,19 @@ def xi_large_estimation(
     xi_zel = xi_zel_call(r_eft)
 
     # Find the ratio between
+    # ==========================================================================
     r_mask = (30 < r_eft) & (r_eft < 50)
-    B_grid = np.linspace(0.8, 1.2, 100)
-    loglike_grid = [loglike_B(b, (xi_eft[r_mask], xi_zel[r_mask])) for b in B_grid]
+    B_grid = np.linspace(0.8, 1.2, 10_000)
+    loglike_grid = [loglike_B(b, (xi_eft[r_mask], xi_zel[r_mask]))
+                    for b in B_grid]
     B_max = B_grid[np.argmax(loglike_grid)]
+    # ==========================================================================
 
+    # Construct xi large
     erf_transition = error_func_pos_incr(r_eft, 1.0, 40.0, 3.0)
 
-    xi_large = (1.0 - erf_transition) * B_max * xi_zel + erf_transition * xi_eft
+    xi_large = (1.0 - erf_transition) * B_max * xi_zel + \
+        erf_transition * xi_eft
 
     # Return all quantities
     if large_only:
