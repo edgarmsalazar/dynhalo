dbb34c8cf7866afd44fc18b4aece896eb8db6505
Edgar Salazar <edgarmsc@arizona.edu>
2025-02-03 18:46:45 -0700

    removed distance metric and added characteristic density

--- dynhalo/finder/coordinates.py
+++ dynhalo/finder/coordinates.py
@@ -82,37 +82,27 @@ def get_vr_vt_from_coordinates(
     return vr, vt, v2
 
 
-# Deprecated
-def distance_metric(
-    rel_pos: np.ndarray,
-    rel_vel: np.ndarray,
-    sigma_x_sq: float,
-    sigma_v_sq: float,
-    lamb: float = 1.0,
+def characteristic_density(
+    r200: float,
+    rs: float
 ) -> float:
-    """Computes the distance between particles and halo centre in
-    six-dimensional phase space.
+    """Computes the characteristic density of an NFW profile.
 
     Parameters
     ----------
-    rel_pos : np.ndarray
-        Relative position of paticles to halo
-    rel_vel : np.ndarray
-        Relative velocity of paticles to halo
-    sigma_x_sq : float
-        Position dispersion parameter 
-    sigma_v_sq : float
-        Velocity dispersion parameter 
-    lamb : float, optional
-        Scaling parameter in distance metric, by default 1.0
+    r200 : float
+        Halo radius
+    rs : float
+        Scale radius
 
     Returns
     -------
     float
-        Six-dimensional distance
+        concentration and delta characteristic
     """
-    return np.sum(np.square(rel_pos), axis=1) / sigma_x_sq + \
-            lamb * np.sum(np.square(rel_vel), axis=1) / sigma_v_sq
+    c200 = r200 / rs
+    delta = ((200./3.) * c200) / (np.log(1 + c200) - (c200 / (1 + c200)))
+    return c200, delta
 
 
 if __name__ == "__main__":
